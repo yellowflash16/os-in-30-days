4f310f52d54b0836a94d2da833364252ed596954
yellowflash16 <ariko9635@gmail.com>
2025-08-08 00:21:30 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,5 +5,6 @@ set extensions="-smart+tex_math_dollars+implicit_figures+link_attributes+tex_mat
 
 pandoc html\book.md -fmarkdown%extensions% -o html/book.html --ascii
 copy web\template.html web\book.html > web\index.html
-rm html\book.html
-rm html\book.md
+Remove-Item html\book.html
+Remove-Item html\book.md
+
